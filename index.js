596cf7bff8c4fb67ff10990f7debc4d777d5a999
slumfrog <kishan.anil@gmail.com>
2019-08-11 19:15:30 +0100

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+// const inventory = [
+//   {name: 'apples', quantity: 2},
+//   {name: 'bananas', quantity: 0},
+//   {name: 'cherries', quantity: 5}
+// ];
+
+
+// const result = inventory.find( fruit => fruit.name === 'cherries' );
+
+function superbowlWin(array) {
+  const win = array.find(win => win.result === "W" )
+  if (win) {return win.year}
+}
