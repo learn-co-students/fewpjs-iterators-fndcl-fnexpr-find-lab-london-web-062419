f6430d35ed25dccac29bb4df354c5708ee92d13b
Anne Marie O'Sullivan <annemarieos.irl@gmail.com>
2019-08-18 14:04:15 +0100

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+const superbowlWin = (array) => {
+  // array.find( record => record.result == "W")
+  const winningRecord = array.find(record => record.result == "W")
+  return !!winningRecord ? winningRecord.year : undefined
 }
